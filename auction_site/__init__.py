14f77bdf203aeb4dee02806c102c453fe6e42d52
Tomasz Kordiak <dotomika@gmail.com>
2020-11-24 12:09:47 +0100

    Remove instance patch

--- auction_site/__init__.py
+++ auction_site/__init__.py
@@ -11,10 +11,8 @@ from . import routes
 
 from config import Config, AdminModelView
 
-patch = os.path.join(os.path.abspath(__file__), '../instance')
-app = Flask(__name__, instance_relative_config=True, instance_path=patch)
+app = Flask(__name__)
 app.config.from_object(Config)
-app.config.from_pyfile('config.py', silent=True)
 
 routes.api.init_app(app)
 
--- config.py
+++ config.py
@@ -6,8 +6,9 @@ from flask import redirect, url_for
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
-    SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(BASE_DIR, 'data.db'))
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or ('sqlite:///' + os.path.join(BASE_DIR, 'data.db')) 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or None
 
 
 class AdminModelView(ModelView):
